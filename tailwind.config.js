8dcfc3483d935b26dbf53b1d92ec09befaf3bcc7
Parminder singh <154919792+Parminder-singh-97@users.noreply.github.com>
2024-12-24 20:33:05 +0530

    new config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
-module.exports = {
-  plugins: [require("preline/plugin")],
-};
+import preline from "preline/plugin";
 
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-
     "node_modules/preline/dist/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("preline/plugin")],
+  plugins: [preline],
 };
